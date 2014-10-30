2bbecf9cb9ac38b7dae9366e2cd843d1db6dfd9d
indranilm <indranil.mustafee@cgi.com>
2014-10-30 13:52:58 +0000

    Deletion to use DELETE http request

--- CHARMe_plugin/src/plugin/js/charme.logic.js
+++ CHARMe_plugin/src/plugin/js/charme.logic.js
@@ -94,6 +94,9 @@ charme.logic.urls.createRequest = function() {
 charme.logic.urls.stateRequest = function(newState) {
 	return charme.logic.urls._baseURL() + 'advance_status/';
 };
+charme.logic.urls.deleteRequest = function(annoID) {
+    return charme.logic.urls._baseURL() + 'resource/' + annoID;
+};
 
 charme.logic.urls.fetchForTarget = function(targetId) {
 	//return 'testData/charmetest.atom';
@@ -815,7 +818,29 @@ charme.logic.searchAnnotations = function(criteria) {
  */
 charme.logic.deleteAnnotation=function(annotationId, token){
 	//return charme.logic.advanceState(annotationId, charme.logic.constants.STATE_DELETE, token)
-	return charme.logic.advanceState(annotationId, 'retired', token);
+	//return charme.logic.advanceState(annotationId, 'retired', token);
+
+    var shortId = charme.logic.shortAnnoId(annotationId);
+
+    var url = charme.logic.urls.deleteRequest(shortId);
+    return new Promise(function(resolver) {
+        $.ajax(url, {
+            dataType: 'xml',
+            type: 'DELETE',
+            headers : {
+                'Authorization' : ' Bearer ' + token
+            },
+            contentType: 'application/json'
+            //data: JSON.stringify({annotation: annotationId, toState: newState})
+        }).then(function(result){
+                resolver.fulfill(result);
+            },
+            function(error){
+                resolver.reject(error);
+            });
+    });
+
+
 };
 
 /*
